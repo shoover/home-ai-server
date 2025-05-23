2811fadcf1f4c1739370ccf7e2485208acc293b3
Shawn Hoover <shawn.hoover@gmail.com>
2025-05-23 15:40:42 -0400

    Restart only specified services if any passed on the command line

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-# Restarts containers with updated docker images.
+# Restarts containers with updated docker images. Targets all
+# services by default, or only those specified as arguments.
 
-docker compose up --detach --pull always --build
+docker compose up --detach --pull always --build "$@"
